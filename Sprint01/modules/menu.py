e0afc2da9e1666a2db70289f66ec6444d62b0617
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-19 22:14:15 +0800

    test menu.py

--- Sprint01/modules/menu.py
+++ Sprint01/modules/menu.py
@@ -1,1 +1,10 @@
+class Menu:
+    def __init__(self):
+        self.items = {
+            "Burger": 5.99,
+            "Pizza": 7.99,
+            "Pasta": 6.49
+        }
 
+    def get_items(self):
+        return self.items
