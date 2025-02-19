d5ef9680a738ff7cb82ea3aa2fb7d30ba0ad0c31
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-19 22:15:32 +0800

    test test_order.py

--- Sprint01/tests/test_order.py
+++ Sprint01/tests/test_order.py
@@ -1,1 +1,11 @@
+import unittest
+from modules.order import Order
 
+class TestOrder(unittest.TestCase):
+    def test_add_order(self):
+        order = Order()
+        order.add_order("Pizza")
+        self.assertIn("Pizza", order.get_orders())
+
+if __name__ == "__main__":
+    unittest.main()
