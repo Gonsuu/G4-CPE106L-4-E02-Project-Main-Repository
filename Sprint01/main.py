b8dc5bca2994a3b03fc8c858669a1a377954a3f0
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-19 22:14:57 +0800

    test main.py

--- Sprint01/main.py
+++ Sprint01/main.py
@@ -1,1 +1,10 @@
+from modules.menu import Menu
+from modules.order import Order
 
+menu = Menu()
+order = Order()
+
+print("Menu:", menu.get_items())
+
+order.add_order("Burger")
+print("Your Orders:", order.get_orders())
