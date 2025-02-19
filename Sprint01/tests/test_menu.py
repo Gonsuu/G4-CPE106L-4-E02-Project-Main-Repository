6ad2978bcd765fee9ae208ebf715709f0a6c63bb
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-19 22:15:19 +0800

    test test_menu.py

--- Sprint01/tests/test_menu.py
+++ Sprint01/tests/test_menu.py
@@ -1,1 +1,10 @@
+import unittest
+from modules.menu import Menu
 
+class TestMenu(unittest.TestCase):
+    def test_get_items(self):
+        menu = Menu()
+        self.assertIn("Burger", menu.get_items())
+
+if __name__ == "__main__":
+    unittest.main()
