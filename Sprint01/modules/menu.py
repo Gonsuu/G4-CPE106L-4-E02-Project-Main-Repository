b2d72ac95a270d4f06244980a4da9e206b9cd929
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-20 13:34:43 +0800

    Updated menu.py

--- Sprint01/modules/menu.py
+++ Sprint01/modules/menu.py
@@ -1,10 +1,39 @@
+#Menu module
 class Menu:
     def __init__(self):
-        self.items = {
-            "Burger": 5.99,
-            "Pizza": 7.99,
-            "Pasta": 6.49
-        }
+        self.menu_items = []
+        self.book_table = []
+        self.customer_orders = []
 
-    def get_items(self):
-        return self.items
+    def add_item_to_menu(self, item, price, tag):
+        self.menu_items.append({"Name": item, "Price": price, "Tag": tag})
+
+    def print_menu(self):
+        print("-"*30)
+        print("Quick-Eats Menu")
+        print("-"*30)
+        for item in self.menu_items:
+            print(f"#{item['Tag']} - {item['Name']} - {item['Price']}")
+        print("-"*30)
+
+    def find_item(self, tag):
+        return next((item for item in self.menu_items if item["Tag"] == tag), None)
+
+menu = Menu()
+
+# menu items
+menu_list = [
+    {"Name": "Classic Cheeseburger", "Price": "₱95", "Tag": 1},
+    {"Name": "Margherita Pizza", "Price": "₱440", "Tag": 2},
+    {"Name": "Grilled Chicken Caesar Salad", "Price": "₱200", "Tag": 3},
+    {"Name": "Spicy Chicken Tacos (2pcs)", "Price": "₱140", "Tag": 4},
+    {"Name": "Vegetable Stir-Fry", "Price": "₱140", "Tag": 5},
+    {"Name": "Fish and Chips", "Price": "₱120", "Tag": 6},
+    {"Name": "Mushroom Risotto", "Price": "₱180", "Tag": 7},
+    {"Name": "BBQ Pulled Pork Sandwich", "Price": "₱120", "Tag": 8},
+    {"Name": "Caprese Panini", "Price": "₱80", "Tag": 9},
+    {"Name": "Chocolate Lava Cake", "Price": "₱200", "Tag": 10},
+]
+
+for item in menu_list:
+    menu.add_item_to_menu(item["Name"], item["Price"], item["Tag"])
