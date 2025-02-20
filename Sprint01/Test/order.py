4bb986ef61f56da7191b0403df10f9626395c02d
Gonsuu <131893474+Gonsuu@users.noreply.github.com>
2025-02-20 23:27:21 +0800

    Delete Sprint01/Test directory

--- Sprint01/Test/order.py
+++
@@ -1,9 +0,0 @@
-class Order:
-    def __init__(self):
-        self.orders = []
-
-    def add_order(self, item):
-        self.orders.append(item)
-
-    def get_orders(self):
-        return self.orders
